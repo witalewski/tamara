989aac63facfc5e0a8f672b5f479c2adb65ef7e8
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-10-19 13:04:53 +0200

    fix App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import main from './reducers'
 import App from './App'
 
 it('renders without crashing', () => {
+  const store = createStore(main)
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
